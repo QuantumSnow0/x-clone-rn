d35c75210ed65bb9e3915ffa1a4403ca42f90fa1
QuantumSnow0 <bmuthuri93@gmail.com>
2025-07-22 20:16:55 +0300

    vercel config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
-import userRoutes from "../routes/user.route.js";
-import postRoutes from "../routes/post.route.js";
-import commentRoutes from "../routes/comment.route.js";
-import notificationRoutes from "../routes/notification.route.js";
-import { ENV } from "../config/env.js";
-import { connectDB } from "../config/db.js";
-import { arcjetMiddleware } from "../middleware/arcjet.middleware.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 const app = express();
 const { PORT } = ENV;
 app.use(express.json());
@@ -26,12 +26,15 @@ app.use((err, req, res, next) => {
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(PORT, "0.0.0.0", () => {
-      console.log(`Server is running on port http://localhost:${PORT}`);
-    });
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(PORT, "0.0.0.0", () => {
+        console.log(`Server is running on port http://localhost:${PORT}`);
+      });
+    }
   } catch (error) {
     console.log("failed to start server".error);
     process.exit(1);
   }
 };
 startServer();
+export default app;
