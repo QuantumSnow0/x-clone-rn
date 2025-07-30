5593eef383f324aafa89c06602566f8e277b8a7a
QuantumSnow0 <bmuthuri93@gmail.com>
2025-07-30 22:00:31 +0300

    auth implemented

---
+++ mobile/app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text, Button } from "react-native";
+import React from "react";
+import { useClerk } from "@clerk/clerk-expo";
+const HomeScreen = () => {
+  const { signOut } = useClerk();
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <Button onPress={() => signOut()} title="Logout" />
+    </View>
+  );
+};
+
+export default HomeScreen;
